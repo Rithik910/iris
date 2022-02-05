f4086a1b4ec019f044fd640f31c88095b60b7931
Rithik910 <97841946+Rithik910@users.noreply.github.com>
2022-02-05 23:52:49 +0530

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-import joblib
-model = joblib.load('iris')       
-st.title('Iris flower Classifier')           
-ip = st.text_input('enter sepal_length,sepal_width,petal_length,petal_width')
-op = model.predict([ip])
-if st.button('Predict'):
-	st.title(op[0])
