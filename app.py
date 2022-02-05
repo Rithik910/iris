a3bbb84305d6b4d4e5e8e1b745fee319ca8ebf12
Rithik910 <97841946+Rithik910@users.noreply.github.com>
2022-02-05 23:43:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import joblib
-model = joblib.load('iris')
-st.title('Iris flower Classifier')
+model = joblib.load('iris')       
+st.title('Iris flower Classifier')           
 ip = st.text_input('enter sepal_length,sepal_width,petal_length,petal_width')
 op = model.predict([ip])
 if st.button('Predict'):
