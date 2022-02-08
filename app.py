7aad49859fcd812de5ada897babc6aeb3fd7c396
Rithik910 <97841946+Rithik910@users.noreply.github.com>
2022-02-08 22:23:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,17 @@ ip = st.text_input('enter sepal_length')
 ip1 = st.text_input('enter sepal_width')
 ip2 = st.text_input('enter petal_length')
 ip3 = st.text_input('enter petal_width')
-op = model.predict([[float(ip),float(ip1),float(ip2),float(ip3)]])
+
 if st.button('Predict'):
-	st.title(op[0])
+	if(ip!="" and ip1!="" and ip2!="" and ip3!=""):
+		op = model.predict([[float(ip),float(ip1),float(ip2),float(ip3)]])
+		if op== 0:
+			st.title("Iris-setosa")
+		elif op== 1:
+			st.title("Iris-versicolor")
+		elif op== 2:
+			st.title("Iris-verginica")
+		
+else:
+	print("Program ended")
+
